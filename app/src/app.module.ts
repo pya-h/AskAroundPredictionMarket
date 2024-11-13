e8beb774739739e8a8ed6acd9091cf794ecce1bc
pya-h <p4ya.gcc@gmail.com>
2024-11-13 18:50:21 +0330

    remove scanner.js

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -17,7 +17,7 @@ import { ConfigService } from './config/config.service';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get('DATABASE_HOST'),
-        port: +configService.get('DATABASE_PORT'),
+        port: +configService.get<number>('DATABASE_PORT'),
         username: configService.get('DATABASE_USER'),
         password: configService.get('DATABASE_PASSWORD'),
         database: configService.get('DATABASE_NAME'),
